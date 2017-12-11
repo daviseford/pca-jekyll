6e6f7e0cc33e820c993381b04c220c3f2f3b5d9a
Davis Ford <ford_davis@bah.com>
2017-12-11 12:17:54 -0500

    first commit

---
+++ public/js/giphy.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  const q = ['fail', 'dumb', 'silly',
+    'funny', 'hilarious', 'error', 'lol'][new Date().getDay()]; // semi-random search query
+  const api_key = 'dc6zaTOxFJmzC';    // your giphy API key
+  let request = new XMLHttpRequest;
+  request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=' + api_key + '&tag=' + q, true);
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText).data.image_url;
+      // console.log(data)
+      const str = '<img src="' + data + '"  title="GIF via Giphy" class="img-fluid d-block w-100 mb-3">';
+      $("#giphyme").append(str)
+    } else {
+      console.log('Reached giphy, but API returned an error');
+    }
+  };
+  request.onerror = function () {
+    console.log('Connection error');
+  };
+  request.send();
+
+});
---
+++ public/js/hours.js
@@ -0,0 +1,24 @@
+// Use this file to dynamically highlight a list of days/hours for PCA
+$(document).ready(function () {
+  const d = getESTDate(); // Date in EST time
+  const day_num = d.getDay(); // 0 is Sunday
+  const hr_num = d.getHours();
+  const s = $('.opening-hours li').eq(day_num)
+
+  s.addClass('active'); // Add active class to the day of the week in the list
+
+  const day_check = day_num > 0 && day_num < 6  // not mon or sun
+  const hr_check = hr_num > 8 && hr_num < 17  // 9 - 5
+  if (day_check && hr_check) {  // Prepend the Open badge to the hour span
+    const inner_str = '<span class="d-none d-sm-inline"> Now</span>'  // Hide the 'Now' on small screens
+    const str = '<span class="ml-3 badge badge-success">Open' + inner_str + '</span>';
+    s.children().before(str)
+  }
+});
+
+function getESTDate() {
+  const offset = -5.0 // EST
+  const clientDate = new Date();
+  const utc = clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000);
+  return new Date(utc + (3600000 * offset));
+}
