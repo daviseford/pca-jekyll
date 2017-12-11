a4815abd94f4d73fa4c2a492bf6f172bea4122a0
Davis Ford <ford_davis@bah.com>
2017-12-11 13:26:38 -0500

    untrack

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Compress images - uncomment to run
-find ./public/images/ -type f -iname '*.jpg'  -exec jpegoptim --strip-com --max=85 {} \;
-find ./public/images/ -type f -iname '*.JPG'  -exec jpegoptim --strip-com --max=85 {} \;
-find ./public/images/ -type f -iname '*.jpeg'  -exec jpegoptim --strip-com --max=85 {} \;
-find ./public/images/ -type f -iname '*.png'  -print0 | xargs -0 optipng -o7
+#find ./public/images/ -type f -iname '*.jpg'  -exec jpegoptim --strip-com --max=85 {} \;
+#find ./public/images/ -type f -iname '*.JPG'  -exec jpegoptim --strip-com --max=85 {} \;
+#find ./public/images/ -type f -iname '*.jpeg'  -exec jpegoptim --strip-com --max=85 {} \;
+#find ./public/images/ -type f -iname '*.png'  -print0 | xargs -0 optipng -o7
 
 # Build with Jekyll
 bundle exec jekyll build
