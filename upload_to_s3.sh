7d66c88de77f4c89c474bd411e3baf77d1f512a7
Davis Ford <ford_davis@bah.com>
2017-12-11 13:24:45 -0500

    push to site

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Compress images - uncomment to run
-#find ./public/images/ -type f -iname '*.jpg'  -exec jpegoptim --strip-com --max=85 {} \;
-#find ./public/images/ -type f -iname '*.JPG'  -exec jpegoptim --strip-com --max=85 {} \;
-#find ./public/images/ -type f -iname '*.jpeg'  -exec jpegoptim --strip-com --max=85 {} \;
-#find ./public/images/ -type f -iname '*.png'  -print0 | xargs -0 optipng -o7
+find ./public/images/ -type f -iname '*.jpg'  -exec jpegoptim --strip-com --max=85 {} \;
+find ./public/images/ -type f -iname '*.JPG'  -exec jpegoptim --strip-com --max=85 {} \;
+find ./public/images/ -type f -iname '*.jpeg'  -exec jpegoptim --strip-com --max=85 {} \;
+find ./public/images/ -type f -iname '*.png'  -print0 | xargs -0 optipng -o7
 
 # Build with Jekyll
 bundle exec jekyll build
 
 # Upload to S3
-aws s3 sync --delete --size-only . s3://parkcenterautomotive.com/ ./_site/
+aws s3 sync --delete --size-only ./_site/ s3://parkcenterautomotive.com/
